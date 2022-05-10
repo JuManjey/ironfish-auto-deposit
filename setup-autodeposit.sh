383faf94f37c08738703815c84e2db7ed489b606
JuManji <opghld@gmail.com>
2022-05-10 17:56:44 +0300

    Update setup-autodeposit.sh

--- setup-autodeposit.sh
+++ setup-autodeposit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd~
 touch /root/autodeposit.sh
-#!/bin/bash | tee -a /root/autodeposit.sh
+echo "#!/bin/bash" | tee -a /root/autodeposit.sh
+yarn --cwd ~/ironfish/ironfish-cli/ start:once deposit | tee -a /root/logs-ironfish/logfile.log
 yarn --cwd ~/ironfish/ironfish-cli/ start:once config:get blockGraffiti | tee -a /root/autodeposit.sh
 yarn --cwd ~/ironfish/ironfish-cli/ start:once deposit | tee -a /root/autodeposit.sh
 mkdir logs-ironfish
 touch /root/logs-ironfish/logfile.log
-* * * * * /root/autodeposit.sh | tee -a /root/logs-ironfish/logfile.log
+* * * * * root /root/autodeposit.sh | tee -a /root/logs-ironfish/logfile.log
 # tail -F /root/logs-ironfish/logfile.log
