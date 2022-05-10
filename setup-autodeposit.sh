91b9d31fd407f80929075063d8091f3e0c515be9
JuManji <opghld@gmail.com>
2022-05-10 12:37:41 +0300

    Update setup-autodeposit.sh

--- setup-autodeposit.sh
+++ setup-autodeposit.sh
@@ -2,4 +2,4 @@
 cd~
 mkdir logs-ironfish
 touch /root/logs-ironfish/logfile.log
-1 * * * * /root/autodeposit.sh | tee -a /root/logs-ironfish/logfile.log
+* * * * * /root/autodeposit.sh | tee -a /root/logs-ironfish/logfile.log
