a83fc62fc151c77f3f6584f9ee5334a71c82bfa4
JuManji <opghld@gmail.com>
2022-05-13 22:52:40 +0300

    Update setup-autodeposit.sh

--- setup-autodeposit.sh
+++ setup-autodeposit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 touch /root/logfile.log
-STR="*/10 * * * * root yarn --cwd ~/ironfish/ironfish-cli/ start deposit --confirm"
-echo "$STR | tee -a /root/logfile.log" | tee -a /etc/crontab
+STR="*/10 * * * * root yarn --cwd ~/ironfish/ironfish-cli/ start deposit --confirm | tee -a /root/logfile.log"
+echo $STR | tee -a /etc/crontab
 systemctl restart cron
 # tail -F /root/logfile.log
